6065338b2c7ded310d2897edadfe7c0640610f4f
SedoyDT <mazzzfaka@gmail.com>
2019-09-26 11:49:11 +0300

    W

--- NodeFull/2/3/refs/path_ref.js
+++ NodeFull/2/3/refs/path_ref.js
@@ -1,11 +1,15 @@
 const path = require('path')
 
-console.log(__filename)
-console.log(path.basename(__filename))
-console.log(path.dirname(__filename))
-console.log(path.extname(__filename))
+console.log(__filename) // аюсолютный путь 
+console.log(path.basename(__filename)) // имя файла
+console.log(path.dirname(__filename)) // адрес папки с файлом
+console.log(path.extname(__filename)) // расширение данного файла 
 
-console.log(path.parse(__filename))
+console.log(path.parse(__filename))  // выведет объект { root: 'D:\\',
+                                      // dir:  'D:\\Projects-Folder\\september\\againjs\\nodefull\\2\\3\\refs',
+                                         // base: 'path_ref.js',
+                                          // ext: '.js',
+                                             // name: 'path_ref' }
 
-console.log(path.join(__dirname, 'test', 'second.html'))
-console.log(path.resolve(__dirname, './test', '/second.html'))
+ console.log(path.join(__dirname, 'test', 'second.html'))
+// console.log(path.resolve(__dirname, './test', '/second.html'))
